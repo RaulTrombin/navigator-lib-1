0aaafc12dda0b07e168247270080dc151af27a55
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-03 10:55:43 -0300

    Add example for use on Python

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,6 +1,36 @@
+import os
 import navigator
 
-tire = navigator.create_random_tire()
-print(f'Tire pressure: {tire.pressure}')
-print(f'Tire material: {tire.material}')
-print(f'Tire size: {tire.size.width}w, {tire.size.height}h')
+print(f"Imports available: {print(navigator.__all__)}")
+
+if os.environ.get("CI") == "true":
+    print("Running in CI")
+    print("Not possible to test navigator sensors yet.")
+
+else:
+    print("Initializin navigator module.")
+    navigator.init()
+
+    print("Setting leds on!")
+    navigator.set_led_on()
+
+    print(f"Temperature: {navigator.read_temp()}")
+
+    print(f"Pressure: {navigator.read_pressure()}")
+
+    Data = navigator.read_adc()
+    print(
+        f"Data ADC Channels: 1 = {Data.first}, 2 = {Data.second}, 3 = {Data.third}, 4 = {Data.fourth}"
+    )
+
+    Data = navigator.read_mag()
+    print(f"Magnetic field: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    Data = navigator.read_accel()
+    print(f"Acceleration: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    Data = navigator.read_gyro()
+    print(f"Gyroscope: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    print("Setting leds off!")
+    navigator.set_led_off()
